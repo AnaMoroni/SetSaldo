9fdc6b2eeec37649d3514225e71c8a0b524c2ecb
Ana Moroni <993323639karolanakarolini@gmail.com>
2024-11-06 22:12:03 -0400

    Inicio do encapsulamento

---
+++ Conta.cs
@@ -0,0 +1,17 @@
+﻿public class Conta
+{
+    private double Saldo;
+
+    public void SetSaldo(double saldo)
+    {
+        if (saldo >= 0)
+        {
+            Saldo = saldo;
+            Console.WriteLine("Saldo adicionado com sucesso");
+        }
+        else
+        {
+            Console.WriteLine("Saldo inválido");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿Conta c = new Conta();
+
+Console.WriteLine("Informe um valor para adicionar a sua conta.");
+double saldo = double.Parse(Console.ReadLine());
+
+c.SetSaldo(saldo);
